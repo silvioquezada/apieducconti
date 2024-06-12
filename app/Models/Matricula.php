f4f2a8b6c47577a00a0963bdc1027ae462666dbc
“silvioescritorio” <sss.silq@gmail.com>
2024-06-12 11:10:11 -0500

    Mejora API

--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -10,6 +10,8 @@ class Matricula extends Model
     use HasFactory;
     public $timestamps = false;
 
+    protected $casts = [ 'estado_matricula' => 'integer', 'estado_aprobacion' => 'integer' ];
+
     public function getKeyName(){
         return 'cod_matricula';
     }
